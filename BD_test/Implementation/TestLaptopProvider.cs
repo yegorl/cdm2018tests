960b1d86af65e842ff991f6d7b37dafd0138d07b
MahlonaVitalii <maglona2013@gmail.com>
2019-01-17 19:59:09 +0200

    add TestLaptopProvider Implementation

---
+++ BD_test/Implementation/TestLaptopProvider.cs
@@ -0,0 +1,63 @@
+﻿using BD_test.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_test.Implementation
+{
+    public class TestLaptopProvider : ITestLaptopProvider
+    {
+        public void AddLaptop(Laptop laptop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteLaptop(Laptop laptop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Laptop GetLaptop(int code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<LaptopModel> GetLaptops()
+        {
+            List<LaptopModel> Laptop_ = new List<LaptopModel>();
+            try
+            {
+                using (LaptopContext db = new LaptopContext())
+                {
+                    var Lap = db.Laptops;
+                    var Prod = db.Products;
+                    var LP = Lap.Join(Prod,
+                         a => a.model,
+                         b => b.model,
+                         (a, b) => new LaptopModel()
+                         {
+                             ProductMaker = b.maker,
+                             Model = a.model,
+                             CpuSpeed = a.speed,
+                             RamSize = a.ram,
+                             DiscSize = a.DiscSize,
+                             Price = a.price,
+                             ScreenSize = a.screen
+                         }
+                    );
+
+
+                    Laptop_ = LP.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                Console.WriteLine(e.StackTrace);
+            }
+            return Laptop_;
+        }
+    }
+}
--- BD_test/Interfaces/ITestLaptopProvider.cs
+++ BD_test/Interfaces/ITestLaptopProvider.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BD_test.EFProvider;
+using BD_test.Implementation;
 
 namespace BD_test.Interfaces
 {
+
     public interface ITestLaptopProvider
     {
         List<LaptopModel> GetLaptops();
